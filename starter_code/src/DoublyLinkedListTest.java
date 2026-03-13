4d2ac8fd42a8fcd3dfda0907fc19fc755ab6437f
azhu48 <azhu48@wisc.edu>
2026-03-12 22:07:41 -0500

    added tests RemoveFirst RemoveLast Size Set

--- starter_code/src/DoublyLinkedListTest.java
+++ starter_code/src/DoublyLinkedListTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -60,12 +62,17 @@ public class DoublyLinkedListTest {
     // Tests for add
 
     /**
-     * @author
-     * @see
+     * @author Anna Z
+     * @see AddAtIndexOutOfBoundsThrowsException
      */
     @Test
     public void testAddAtIndexOutOfBoundsThrowsException() {
-        fail("Not yet implemented");
+    //    fail("Not yet implemented");
+        if(SHOULD_FAIL){
+            list = new AddAtIndexOutOfBoundsThrowsException<>();
+        }
+        assertThrows(IndexOutOfBoundsException.class, () -> list.add(1, 1));
+        assertThrows(IndexOutOfBoundsException.class, () -> list.add(-1, 1));
     }
 
     // Tests for addFirst
@@ -142,11 +149,86 @@ public class DoublyLinkedListTest {
     }
 
     // Tests for removeFirst
+    /**
+     * @author Anna Z
+     * @see RemoveFirst
+     */
+    @Test
+    public void testRemoveFirst(){
+
+        if(SHOULD_FAIL){
+            list = new RemoveFirst<>();
+        }
+
+        list.addLast(1);
+        list.addLast(2);
+
+        list.removeFirst();
+        assertNotEquals(1, list.getFirst());
+
+    }
 
     // Tests for removeLast
+    /**
+     * @author Anna Z
+     * @see RemoveLast
+     */
+    @Test
+    public void testRemoveFirst(){
+
+        if(SHOULD_FAIL){
+            list = new RemoveLast<>();
+        }
+
+        list.addLast(1);
+        list.addLast(2);
+
+        list.removeLast();
+        assertNotEquals(2, list.getFirst());
+
+    }
 
     // Tests for size
+    /**
+     * @author Anna Z
+     * @see Size
+     */
+    @Test
+    public void testRemoveFirst(){
+
+        if(SHOULD_FAIL){
+            list = new Size<>();
+        }
+
+        assertEquals(0, list.size());
+
+        list.addLast(1);
+        list.addLast(2);
+
+        assertEquals(2, list.size());
+
+    }
 
     // Tests for set
+    /**
+     * @author Anna Z
+     * @see Set
+     */
+    @Test
+    public void testRemoveFirst(){
+
+        if(SHOULD_FAIL){
+            list = new Set<>();
+        }
+
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+
+        list.set(1, 99);
+        assertEquals(1, list.get(0));
+        assertEquals(99, list.get(1));
+
+    }
 
 }
